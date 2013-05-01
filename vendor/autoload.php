53c243ff0ed3bd861c87f2c8e6b880b3042803b4
Rafael Calleja <rafaelcalleja@gmail.com>
2013-05-01 13:33:35 +0200

    [dev]configure phpunit and travis

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1263e1b0b130ed9e1e39ef53169cde8e::getLoader();
+return ComposerAutoloaderInit0b197c0e2c11d38dac8b0823a1b15685::getLoader();
